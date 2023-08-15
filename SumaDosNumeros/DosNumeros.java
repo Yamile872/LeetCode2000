c836d2d0fab62d7d337fe117745a8fe1f06fd87a
Yamile Silva <yamilesilva2000@gmial.com>
2023-08-15 00:52:04 -0500

    comentario

--- SumaDosNumeros/DosNumeros.java
+++ SumaDosNumeros/DosNumeros.java
@@ -1,6 +1,23 @@
 import java.util.HashMap;
 
 public class HashMapSuma{
+
+    public static int[] SumaNumeros(int[] arreglo, int objetivo)
+    {
+        Map <Integer, Integer> mapa = new HashMap();
+        for(int i=0; i<arreglo.length; i++)
+        {
+            int complemento = objetivo - arreglo[i];
+            if(mapa.constainKey(complemento)){
+                return new int[]{
+                    mapa.get(complemento), i
+                };
+            }
+            mapa.put(num[i],i);
+        }
+        return null;
+    }
+
     public static main(String[] args){
         
     }
