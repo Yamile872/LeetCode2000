d10fd528511be0dd5872015179dbd9c583ab6845
Yamile Silva <yamilesilva2000@gmail.com>
2023-08-16 02:13:20 -0500

    Palindromo

---
+++ Palindormo/Palindromo.java
@@ -0,0 +1,18 @@
+public class Palindromo {
+    public static bool NumeroPalindromo (int x)
+    {
+        if(x < 0 || (x != 0 && x % 10 == 0)){
+            return false;
+        }
+
+        int reversa = 0;
+        int original = x;
+
+        while( x > reversa) {
+            reversa = (reversa * 10) + (x % 10);
+            x /= 10; 
+        }
+        
+        return (x == reversa) || (x == reversa / 10);
+    }
+}
